d904ed0ff1f2074a8a239b46941939ebaefdacc7
minh317 <minh317@naver.com>
2023-05-14 14:15:47 +0900

    [feat] HTTP 요청/응답 WrappingFilter 추가

---
+++ src/main/java/com/shopping/mall/config/filter/HttpServletWrappingFilter.java
@@ -0,0 +1,29 @@
+package com.shopping.mall.config.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HttpServletWrappingFilter extends OncePerRequestFilter {
+
+    /**
+     * Request, Response를 Wrapping 하는 이유는?
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper wrappingRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappingResponse = new ContentCachingResponseWrapper(response);
+
+        filterChain.doFilter(wrappingRequest, wrappingResponse);
+
+        wrappingResponse.copyBodyToResponse();
+    }
+}
